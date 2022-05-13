de6128bbd4fd2916344a673f51e828789912b9ac
Matt Travis <matthew.travis@azenta.com>
2022-05-13 21:43:14 +0100

    Extracted Hotel and Flight queries to separate methods. Added Destination Airport filter to Flight query - somehow I was passing the tests only on the departure airport.

--- HolidaySearchKata/HolidaySearchKata/HolidaySearch.cs
+++ HolidaySearchKata/HolidaySearchKata/HolidaySearch.cs
@@ -1,38 +1,51 @@
 ﻿using Newtonsoft.Json;
 
-namespace HolidaySearchKata
+namespace HolidaySearchKata;
+
+public class HolidaySearch
 {
-    public class HolidaySearch
+    private readonly List<Flight> _flights;
+    private readonly List<Hotel> _hotels;
+
+    public HolidaySearch()
     {
-        private readonly List<Flight> _flights;
-        private readonly List<Hotel> _hotels;
+        var directory = Directory.GetCurrentDirectory();
+        var flightPath = Path.Combine(directory, "FlightData.json");
+        var hotelPath = Path.Combine(directory, "HotelData.json");
 
-        public HolidaySearch()
+        _flights = JsonConvert.DeserializeObject<List<Flight>>(File.ReadAllText(flightPath));
+        _hotels = JsonConvert.DeserializeObject<List<Hotel>>(File.ReadAllText(hotelPath));
+    }
+
+    public List<Holiday> PerformSearch(Query query)
+    {
+        var results = new List<Holiday>();
+
+        var flights = FindFlights(query.DepartingFrom, query.TravelingTo, query.DepartureDate);
+
+        var hotels = FindHotels(query.TravelingTo, query.Duration, query.DepartureDate);
+
+        foreach (var hotel in hotels)
         {
-            var directory = Directory.GetCurrentDirectory();
-            var flightPath = Path.Combine(directory, "FlightData.json");
-            var hotelPath = Path.Combine(directory, "HotelData.json");
-
-            _flights = JsonConvert.DeserializeObject<List<Flight>>(File.ReadAllText(flightPath));
-            _hotels = JsonConvert.DeserializeObject<List<Hotel>>(File.ReadAllText(hotelPath));
+            results.AddRange(flights.Select(flight => new Holiday(flight, hotel)));
         }
 
-        public List<Holiday> PerformSearch(Query query)
-        {
-            var results = new List<Holiday>();
+        results = results.OrderBy(x => x.TotalCost).ToList();
 
-            var flights = _flights.Where(x => (x.From & query.DepartingFrom) == x.From && x.DepartureDate == query.DepartureDate).ToList();
+        return results;
+    }
 
-            var hotels = _hotels.Where(x => x.Nights == query.Duration && x.ArrivalDate == query.DepartureDate && (x.LocalAirportsCombined & query.TravelingTo) == query.TravelingTo).ToList();
+    private IEnumerable<Hotel> FindHotels(Airport queryTravelingTo, int queryDuration, DateTime queryDepartureDate)
+    {
+        return _hotels.Where(x => x.Nights == queryDuration 
+                                  && x.ArrivalDate == queryDepartureDate 
+                                  && (x.LocalAirportsCombined & queryTravelingTo) == queryTravelingTo).ToList();
+    }
 
-            foreach (var hotel in hotels)
-            {
-                results.AddRange(flights.Select(flight => new Holiday(flight, hotel)));
-            }
-
-            results = results.OrderBy(x => x.TotalCost).ToList();
-
-            return results;
-        }
+    private List<Flight> FindFlights(Airport queryDepartingFrom, Airport queryTravelingTo, DateTime queryDepartureDate)
+    {
+        return _flights.Where(x => (x.From & queryDepartingFrom) == x.From 
+                                   && (x.To & queryTravelingTo) == x.To 
+                                   && x.DepartureDate == queryDepartureDate).ToList();
     }
 }
--- HolidaySearchKata/HolidaySearchKata/HolidaySearchTests.cs
+++ HolidaySearchKata/HolidaySearchKata/HolidaySearchTests.cs
@@ -1,86 +1,85 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace HolidaySearchKata
+namespace HolidaySearchKata;
+
+[TestClass]
+public class HolidaySearchTests
 {
-    [TestClass]
-    public class HolidaySearchTests
+    [TestMethod]
+    public void UserStory1Test()
     {
-        [TestMethod]
-        public void UserStory1Test()
+        //Input Query
+        //Departing from: Manchester Airport (MAN)
+        //Traveling to: Malaga Airport (AGP)
+        //Departure Date: 2023/07/01
+        //Duration: 7 nights
+        var query = new Query
         {
-            //Input Query
-            //Departing from: Manchester Airport (MAN)
-            //Traveling to: Malaga Airport (AGP)
-            //Departure Date: 2023/07/01
-            //Duration: 7 nights
-            var query = new Query
-            {
-                DepartingFrom = Airport.Man,
-                TravelingTo = Airport.Agp,
-                DepartureDate = new DateTime(2023, 07, 01),
-                Duration = 7
-            };
+            DepartingFrom = Airport.Man,
+            TravelingTo = Airport.Agp,
+            DepartureDate = new DateTime(2023, 07, 01),
+            Duration = 7
+        };
 
-            var holidaySearch = new HolidaySearch();
+        var holidaySearch = new HolidaySearch();
 
-            var results = holidaySearch.PerformSearch(query);
+        var results = holidaySearch.PerformSearch(query);
 
-            //Expectation
-            //Flight 2 and Hotel 9
-            Assert.AreEqual(2, results.First().Flight.Id);
-            Assert.AreEqual(9, results.First().Hotel.Id);
-        }
+        //Expectation
+        //Flight 2 and Hotel 9
+        Assert.AreEqual(2, results.First().Flight.Id);
+        Assert.AreEqual(9, results.First().Hotel.Id);
+    }
         
-        [TestMethod]
-        public void UserStory2Test()
+    [TestMethod]
+    public void UserStory2Test()
+    {
+        //Input Query
+        //Departing from: Any London Airport
+        //Traveling to: Mallorca Airport (PMI)
+        //Departure Date: 2023/06//15
+        //Duration: 10 nights
+        var query = new Query
         {
-            //Input Query
-            //Departing from: Any London Airport
-            //Traveling to: Mallorca Airport (PMI)
-            //Departure Date: 2023/06//15
-            //Duration: 10 nights
-            var query = new Query
-            {
-                DepartingFrom = Airport.AnyLondon,
-                TravelingTo = Airport.Pmi,
-                DepartureDate = new DateTime(2023, 06, 15),
-                Duration = 10
-            };
+            DepartingFrom = Airport.AnyLondon,
+            TravelingTo = Airport.Pmi,
+            DepartureDate = new DateTime(2023, 06, 15),
+            Duration = 10
+        };
 
-            var holidaySearch = new HolidaySearch();
+        var holidaySearch = new HolidaySearch();
 
-            var results = holidaySearch.PerformSearch(query);
+        var results = holidaySearch.PerformSearch(query);
             
-            //Expectation
-            //Flight 6 and Hotel 5
-            Assert.AreEqual(6, results.First().Flight.Id);
-            Assert.AreEqual(5, results.First().Hotel.Id);
-        }
+        //Expectation
+        //Flight 6 and Hotel 5
+        Assert.AreEqual(6, results.First().Flight.Id);
+        Assert.AreEqual(5, results.First().Hotel.Id);
+    }
         
-        [TestMethod]
-        public void UserStory3Test()
+    [TestMethod]
+    public void UserStory3Test()
+    {
+        //Input Query
+        //Departing from: Any Airport
+        //Traveling to: Gran Canaria Airport (LPA)
+        //Departure Date: 2022/11/10
+        //Duration: 14 nights
+        var query = new Query
         {
-            //Input Query
-            //Departing from: Any Airport
-            //Traveling to: Gran Canaria Airport (LPA)
-            //Departure Date: 2022/11/10
-            //Duration: 14 nights
-            var query = new Query
-            {
-                DepartingFrom = Airport.AnyAirport,
-                TravelingTo = Airport.Lpa,
-                DepartureDate = new DateTime(2022, 11, 10),
-                Duration = 14
-            };
+            DepartingFrom = Airport.AnyAirport,
+            TravelingTo = Airport.Lpa,
+            DepartureDate = new DateTime(2022, 11, 10),
+            Duration = 14
+        };
 
-            var holidaySearch = new HolidaySearch();
+        var holidaySearch = new HolidaySearch();
 
-            var results = holidaySearch.PerformSearch(query);
+        var results = holidaySearch.PerformSearch(query);
             
-            //Expectation
-            //Flight 7 and Hotel 6
-            Assert.AreEqual(7, results.First().Flight.Id);
-            Assert.AreEqual(6, results.First().Hotel.Id);
-        }
+        //Expectation
+        //Flight 7 and Hotel 6
+        Assert.AreEqual(7, results.First().Flight.Id);
+        Assert.AreEqual(6, results.First().Hotel.Id);
     }
 }
